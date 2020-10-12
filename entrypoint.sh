ec1bb8173ad1f39537eb3db45b45b801fc5f74fc
Edward Rousseau <erousseau@deloitte.com.au>
2020-10-12 22:45:20 +1100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,36 +2,43 @@
 
 set -e
 
-EXECUTABLE_NAME=$(basename $GITHUB_REPOSITORY)
-RELEASE_ID=$(cat $GITHUB_EVENT_PATH | jq .release.id)
-RELEASE_TAG_NAME=$(cat $GITHUB_EVENT_PATH | jq .release.tag_name)
+
+RELEASE_ID=$(cat $GITHUB_EVENT_PATH | jq -r .release.id)
+RELEASE_TAG_NAME=$(cat $GITHUB_EVENT_PATH | jq -r .release.tag_name)
 UPLOAD_URL="https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID/assets"
 
 GOOSES="linux windows darwin"
 GOARCHES="amd64"
 
-cd $GITHUB_WORKSPACE
 for GOOS in $GOOSES; do
   for GOARCH in $GOARCHES; do
     export GOOS
     export GOARCH
-    if [ $GOOS == "windows" ]; then $EXECUTABLE_NAME="$EXECUTABLE_NAME.exe"; fi
-    go get
+    if test $GOOS = "windows"; then
+      EXECUTABLE_NAME="$(basename $GITHUB_REPOSITORY).exe"
+      ARCHIVE_NAME="${EXECUTABLE_NAME}_${RELEASE_TAG_NAME}_${GOOS}_${GOARCH}.zip"
+    else
+      EXECUTABLE_NAME=$(basename $GITHUB_REPOSITORY)
+      ARCHIVE_NAME="${EXECUTABLE_NAME}_${RELEASE_TAG_NAME}_${GOOS}_${GOARCH}.tar.gz"
+    fi
     go build -o $EXECUTABLE_NAME -tags osusergo,netgo
-    NAME="${EXECUTABLE_NAME}_${RELEASE_TAG_NAME}_${GOOS}_${GOARCH}"
-    tar cvfz $NAME.tar.gz $EXECUTABLE_NAME
-    CHECKSUM=$(md5sum $NAME.tar.gz | cut -d ' ' -f 1)
+    if test $GOOS = "windows"; then
+      zip $ARCHIVE_NAME $EXECUTABLE_NAME
+    else
+      tar cvfz $ARCHIVE_NAME $EXECUTABLE_NAME
+    fi
+    CHECKSUM=$(md5sum $ARCHIVE_NAME | cut -d ' ' -f 1)
     curl \
       -X POST \
-      --data-binary @$NAME.tar.gz \
+      --data-binary @$ARCHIVE_NAME \
       -H 'Content-Type: application/gzip' \
       -H "Authorization: Bearer $GITHUB_TOKEN" \
-      "$UPLOAD_URL?name=$NAME.tar.gz"
+      "$UPLOAD_URL?name=$ARCHIVE_NAME"
     curl \
       -X POST \
       --data $CHECKSUM \
       -H 'Content-Type: text/plain' \
-      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-      "$UPLOAD_URL?name=${NAME}_checksum.txt"
+      -H "Authorization: Bearer $GITHUB_TOKEN" \
+      "$UPLOAD_URL?name=$ARCHIVE_NAME-checksum.txt"
   done
 done
